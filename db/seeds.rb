c720114e4feae2d5c22bd4c5937c87de3ab88645
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-05-03 21:47:46 +0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: "Hello there!")
+Greeting.create(message: "Wagwaan gee")
+Greeting.create(message: "Salam aleikum")
+Greeting.create(message: "Shalom")
+Greeting.create(message: "wys blud")
